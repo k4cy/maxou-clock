606f3affed58b258f23ea731d84157a9f2636813
k4cy <kagoun@gmail.com>
2017-10-01 16:42:01 +0200

    Ajout de test mp3 et liens symbolique vers les mp3

---
+++ bin/test-mp3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from playsound import playsound
+playsound(os.path.realpath(os.path.dirname(__file__))+'/../sounds/sw-imperial-march.mp3')
